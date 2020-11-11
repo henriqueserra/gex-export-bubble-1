366f8a3d754680f852d0b5ed3b4f15ef6a3537ae
Henrique Ribeiro Serra <gex.gerenteexpresso@gmail.com>
2020-11-11 12:16:10 -0300

    Update webserver.js

--- config/webserver.js
+++ config/webserver.js
@@ -4,7 +4,7 @@ module.exports = () => {
     //              Cria Servidor Web
     const customExpress = require('./express.js');
     const app = customExpress();
-    app.listen(3001, () => console.log('servidor rodando na porta 3001'));
+    app.listen(3000, () => console.log('servidor rodando na porta 3000'));
     // ===============================================
     // ===============================================
 };
