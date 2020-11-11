db1a739d60e8e9f9c802527a280b0dc80c6352f4
Henrique Ribeiro Serra <gex.gerenteexpresso@gmail.com>
2020-11-11 15:50:12 -0300

    versao-001.01

--- controllers/consultavisao.js
+++ controllers/consultavisao.js
@@ -2,12 +2,25 @@ const moment = require('moment');
 // const momenttz = require('moment-timezone');
 const ExportVisao = require('../models/model.exportavisao');
 
+
 module.exports = app => {
     // ====================GET======================
 
     app.get('/export-bubble', (req, res) => {
 
-        ExportVisao.find({})
+        ExportVisao.find({}).limit(50)
+          .exec((erro, resultados) => {
+            if (erro) {
+                console.log('Erro lendo Vendas ' + err);
+            } else {
+                res.status(200).json(resultados)
+            }
+        });
+    });
+
+    app.get('/export-bubble/data/:data', (req, res) => {
+
+        ExportVisao.find({criado: {$gt: new Date(req.params.data)}}).limit(50)
           .exec((erro, resultados) => {
             if (erro) {
                 console.log('Erro lendo Vendas ' + err);
